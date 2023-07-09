10021428bc9169e91b986604e0551b67947744d4
akshaygarg09 <137668997+akshaygarg09@users.noreply.github.com>
2023-07-09 10:36:15 +0530

    Add files via upload

---
+++ python varaible.py
@@ -0,0 +1,32 @@
+x="Akshay Kumar"
+y="Btech 3rd year"
+z=2023
+print(x)
+print(y)
+print(z)
+
+
+
+my_varaible_name = "Purusing Btech CSE"
+print(my_varaible_name)
+
+
+Completed = ["Sem1", "Sem2", "Sem3", "Sem4", "Completed"]
+a, b, c, d, e = Completed
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+
+x = "awesome"
+
+def myfunc():
+    global x
+    x = "fantastic"
+    
+myfunc()
+    
+print("Python is " + x)
