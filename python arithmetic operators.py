402dbadf3d7ad26a559577c4b18c434d08db2736
akshaygarg09 <137668997+akshaygarg09@users.noreply.github.com>
2023-07-09 11:07:36 +0530

    Add files via upload

---
+++ python arithmetic operators.py
@@ -0,0 +1,41 @@
+# Arithmetic Operators
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+modulus = x % y
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
+
+# Comparison Operators
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+greater_than = x > y
+less_than = x < y
+equal_to = x == y
+not_equal_to = x != y
+
+print("Greater than:", greater_than)
+print("Less than:", less_than)
+print("Equal to:", equal_to)
+print("Not equal to:", not_equal_to)
+
+# Logical Operators
+a = bool(input("Enter the first boolean value (True/False): "))
+b = bool(input("Enter the second boolean value (True/False): "))
+
+logical_and = a and b
+logical_or = a or b
+logical_not = not a
+
+print("Logical AND:", logical_and)
+print("Logical OR:", logical_or)
+print("Logical NOT:", logical_not)
