e314f551588b814faeb15ba6d032fe4ffe1b96ce
akshaygarg09 <137668997+akshaygarg09@users.noreply.github.com>
2023-07-09 16:44:58 +0530

    Add files via upload

---
+++ python condition.py
@@ -0,0 +1,12 @@
+name = input("Enter your Name: ")
+roll_no = int(input("Enter your Roll No: "))
+
+if name == "akshay":
+    print("Your marks = 90")
+else:
+    print("Your name is incorrect")
+
+if roll_no == 11212:
+    print("Your Section is A")
+else:
+    print("Your Roll No is incorrect")
