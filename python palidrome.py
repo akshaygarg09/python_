1ad0b150dfaf9e7e31ce796dfc37cf34bad6e074
akshaygarg09 <137668997+akshaygarg09@users.noreply.github.com>
2023-07-09 17:51:46 +0530

    Add files via upload

---
+++ python palidrome.py
@@ -0,0 +1,12 @@
+def is_palindrome(string):
+    
+    string = string.replace(" ", "").lower()
+
+    return string == string[::-1]
+
+
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
