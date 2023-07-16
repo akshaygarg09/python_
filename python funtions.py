9b3930e93ee977f65c681a20bd5395321bd1947a
akshaygarg09 <137668997+akshaygarg09@users.noreply.github.com>
2023-07-16 19:03:30 +0530

    Add files via upload

---
+++ python funtions.py
@@ -0,0 +1,26 @@
+
+def add_numbers(a, b):
+    return a + b
+
+
+sum_result = add_numbers(5, 10)
+print(sum_result)  
+  
+  
+  
+def my_function(child3,child2,child1):
+      print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+
+
+
+
+def my_function(x):
+    return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
